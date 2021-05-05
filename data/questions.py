7e56b71a4a9f87ffffd0e2d2e49e40d76d21f55e
Olivier <olivier.elophe@gmail.com>
2021-05-05 09:55:21 +0200

    séparation des questions

---
+++ data/questions.py
@@ -0,0 +1,14 @@
+liste_questions = [
+    ["Quelle est la couleur du chat ?",
+                "rouge",
+                "violet",
+                "orange",
+                "bleu"
+     ],
+    ["Quel est l'age du chat ?",
+                 45,
+                 3,
+                 273,
+                 1
+     ]
+]
--- quiz.py
+++ quiz.py
@@ -1,13 +1,9 @@
 from flask import Flask, render_template, request
-
+from data.questions import liste_questions
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
-    question = ["Quelle est la couleur du chat ?", "rouge", "violet", "orange", "bleu"]
-    question2 = ["Quel est l'age du chat ?", 45, 3, 273, 1]
-    liste_questions = [question, question2]
     return render_template('index.html', nom='Olivier', questions=liste_questions)
 
 @app.route('/resultat', methods=['post'])
